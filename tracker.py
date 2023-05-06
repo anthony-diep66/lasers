c71efc45335e2b16c60405391e9cd0ff1e4536b3
Anthony Diep <anthonydiep@u.boisestate.edu>
2023-05-05 23:51:02 -0600

    untested code

---
+++ tracker.py
@@ -0,0 +1,149 @@
+import cv2 as cv 
+import numpy as np
+import math
+import utils 
+
+class Tracker:
+
+    ix = 0
+    iy = 0
+
+    def __init__(self, serialPort, winName="Display", 
+                 frameRate=25, resourceDir="res/", arduinoWidth=600, 
+                 arduinoHeight = 600, fcascadeXML="frontal_face.xml"):
+        self.serialPort = serialPort
+        self.winName = winName
+        self.frameRate = frameRate 
+        self.resourceDir = resourceDir 
+        self.arduinoWidth = arduinoWidth
+        self.arduinoHeight = arduinoHeight
+        self.faceCascadeXML = fcascadeXML
+
+    def setVidDim(self, cap, defaults=False):
+        if defaults is True:
+            self.vidWidth = self.arduinoWidth
+            self.vidHeight = self.arduinoHeight
+            return
+
+        self.vidHeight = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
+        self.vidWidth = cap.get(cv.CAP_PROP_FRAME_WIDTH)
+        print(f"h={self.vidHeight} w={self.vidWidth}")
+
+    def toArduinoSpecificData(self, pos):
+        return "X" + str(180-math.floor(pos[0]//(self.arduinoWidth/180))) + "Y" + str(180-math.floor(pos[1]//(self.arduinoHeight/180)))
+
+    def sendSerialData(self, packet):
+        print(packet)
+        #self.serialPort.write(packet.encode("utf-8"))
+
+    def scaleAndSend(self, x, y, factor=1, printMode=False):
+        sx = int((self.arduinoWidth * x) // self.vidWidth) * factor
+        sy = int((self.arduinoHeight * y) // self.vidHeight) * factor
+        self.ix, self.iy = x, y
+        if printMode is True:
+            print(f"({sx},{sy})")
+        self.sendSerialData(self.toArduinoSpecificData((sx,sy)))
+
+    def drawCircleOnFrame(self, frame, xy):
+        cv.circle(frame, xy, utils.DEFAULT_CIRCLE_RADIUS, utils.RED, thickness=3)
+
+    def drawRectangleOnFrame(self, frame, gface):
+        cv.rectangle(frame, (gface[0],gface[1]), 
+                     (gface[0]+gface[2],gface[1]+gface[3]), utils.RED, thickness=3)
+
+    def trackMouse(self, event, x, y, flags, param):
+        self.scaleAndSend(x, y, param[0], param[1])
+
+    def quit(self, frameRate):
+        if cv.waitKey(frameRate) == ord("q"):
+            return True
+        return False
+
+    def track(self):
+        raise NotImplementedError() 
+
+class MouseTracker(Tracker):
+
+    def __init__(self, serialPort):
+        super().__init__(serialPort)
+
+    def track(self, path):
+        cap = cv.VideoCapture(path)
+        self.setVidDim(cap)
+        cv.namedWindow(self.winName, cv.WINDOW_GUI_EXPANDED) 
+        if path == 0:
+            frameResizeFactor = 1
+        else:
+            frameResizeFactor = 0.5
+        cv.setMouseCallback(self.winName, self.trackMouse, param=(int(1/frameResizeFactor), True))
+        while cap.isOpened():
+            ret, frame = cap.read()
+            assert ret is not None 
+
+            # rescale smaller to improve performance 
+            frame = utils.rescaleFrame(frame, scale=frameResizeFactor)
+
+            self.drawCircleOnFrame(frame, (self.ix, self.iy))
+
+            cv.imshow(self.winName, frame) 
+            if self.quit(self.frameRate) is True:
+                break
+        
+        cap.release()
+        cv.destroyAllWindows()
+    
+    def trackBlackScreen(self):
+        blackScreen = np.zeros((600,600,3), np.uint8)
+        self.setVidDim(None, defaults=True)
+        cv.imshow(self.winName, blackScreen)
+        cv.setMouseCallback(self.winName, self.trackMouse, (1, True))
+        if self.quit(0) is True:
+            return
+
+class FaceTracker(Tracker):
+
+    def __init__(self, serialPort):
+        super().__init__(serialPort)
+        self.faceCascade = cv.CascadeClassifier(self.faceCascadeXML)
+
+    def track(self):
+        cap = cv.VideoCapture(utils.LIVE_VIDEO)
+        self.setVidDim(cap)
+        cv.namedWindow(self.winName) 
+        while cap.isOpened():
+            ret, frame = cap.read()
+            assert ret is not None 
+
+            # convert to grayscale
+            gframe = utils.gray(frame)
+
+            # detect faces
+            gfaces = self.faceCascade.detectMultiScale(gframe, scaleFactor=1.1, minNeighbors=3, minSize=[50,50])
+
+            # if faces are detected
+            if len(gfaces) >= 1:
+                gface = gfaces[0]
+
+                # calculates middle of drawn box
+                middle = ((gface[0] + gface[2]//2), ((gface[1] + gface[3]//2)))
+                self.scaleAndSend(middle[0], middle[1], factor=1, printMode=False)
+                self.drawRectangleOnFrame(frame, gface)
+                self.drawCircleOnFrame(frame, middle)
+
+            cv.imshow(self.winName, frame) 
+            if self.quit(self.frameRate) is True:
+                break
+        
+        cap.release()
+        cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    #ports = utils.serial.tools.list_ports.comports()
+    #templateSerialPort = utils.serial.Serial()
+    #serialPort = utils.initSerialPorts(ports, templateSerialPort)
+    #serialPort.open()
+    tracker = FaceTracker(1)
+    tracker.track()
+    #serialPort.close()
+
+    
--- utils.py
+++ utils.py
@@ -1,4 +1,22 @@
 import cv2 as cv
+import serial.tools.list_ports
+from time import sleep 
+import math 
+
+RED = (0, 0, 255)
+GREEN = (0, 255, 0)
+BLUE = (255, 0, 0)
+DEFAULT_CIRCLE_RADIUS = 2
+LIVE_VIDEO = 0
+BAUDRATE = 9600
+
+def getComPortNumber(token):
+    num = ""
+    for c in str(token):
+        if c.isnumeric() == True:
+            num += c
+    val = "COM" + num
+    return val
 
 def rescaleFrame(frame, scale=0.6):
     height = int(frame.shape[0] * scale)
@@ -14,3 +32,26 @@ def rescaleDim(dim, scale=0.6):
 def gray(frame):
     return cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
+def initSerialPorts(ports, serial_port):
+    plist = []
+
+    for port in ports:
+        plist.append(str(port))
+
+    port = None
+    if len(plist) == 0:
+        print("No ports available")
+        exit()
+    if len(plist) == 1:
+        port = getComPortNumber(str(plist.pop()).split().pop()) # take the port name, split to token, get the first token and pass to function
+    else:
+        for i in range(0, len(plist)):
+            print(str(i) + ": " + str(plist[i]))
+        usrinput = int(input(("Multiple ports available, choose one: ")))
+        port = getComPortNumber(str(plist.pop(usrinput)).split().pop())
+
+    print("Chosen port: " + str(port))
+
+    serial_port.baudrate = BAUDRATE
+    serial_port.port = port
+    return serial_port
